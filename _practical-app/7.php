7e474e3cb1b94bbc7794dacee152705d0668d660
SparksFullStack <csparks@antioch.edu>
2019-01-16 15:56:16 -0800

    completed exercise 7

--- _practical-app/7.php
+++ _practical-app/7.php
@@ -16,7 +16,7 @@
 	
 	
 	
-	<?php  
+<?php  
 
 	/*  Step 1 - Create a database in PHPmyadmin
 
@@ -27,8 +27,28 @@
 		Step 4 - Connect to Database and read data
 
 */
+
+	$connection = mysqli_connect('localhost', 'root', '', '7');
+	if (!$connection) die("there was an error connecting to the database");
 	
-	?>
+	// $query = "
+	// 	INSERT INTO users(username, password)
+	// 	VALUES('cheesedick', 'dickcheese')
+	// ";
+	// $result = mysqli_query($connection, $query);
+	// if (!$result) die("there was an error adding data to the database");
+
+	$readQuery = "
+		SELECT * FROM users WHERE username='cheesedick'
+	";
+
+	$readResult = mysqli_query($connection, $readQuery);
+	while($row = mysqli_fetch_assoc($readResult)){
+		$newPass = $row['password'];
+		echo $newPass;
+	};
+
+?>
 
 
 
