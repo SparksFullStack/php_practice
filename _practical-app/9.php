c5b63008a562f28ee4327b65b532e67c46fc12a0
SparksFullStack <csparks@antioch.edu>
2019-01-16 18:39:44 -0800

    completed practice section 9

--- _practical-app/9.php
+++ _practical-app/9.php
@@ -15,10 +15,10 @@
 
 			<article class="main-content col-xs-8">
 			
-		
+		<a href="9.php?test=example">CLICK HERE</a>
 	
 	<?php 
-
+		// echo $_GET["test"];
 	/*  Create a link saying Click Here, and set 
 	the link href to pass some parameters and use the GET super global to see it
 
@@ -26,7 +26,16 @@
 
 		Step 3 - Start a session and set it to value, any value you want.
 	*/
-	
+		$cookieName = "examplecookie";
+		$cookieVal = "some value";
+		$cookeExp = time() + (60*60*7*24*2);
+		setcookie($cookieName, $cookieVal, $cookeExp);
+		echo $_COOKIE['examplecookie'];
+
+
+		session_start();
+		$_SESSION['cheese'] = 'ballz';
+		echo $_SESSION['cheese'];
 	?>
 
 
